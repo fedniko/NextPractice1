824501d1f0af3c2b4fe2fcb3fee7d0843f8498d0
fedniko <fednik2011@gmail.com>
2021-03-03 20:24:59 +0900

    first commit

---
+++ pages/about/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Router from 'next/router'
+import {MainLayout} from "../../components/MainLayout";
+import Head from "next/head";
+
+export default function About({title}) {
+    const linkClickHandler = () => {
+        Router.push('/')
+    }
+
+    return (
+        <MainLayout title={'About page'}>
+            <h1>{title}</h1>
+            <button onClick={linkClickHandler}>Go back to home</button>
+            <button onClick={() => Router.push('/posts')}>Go to posts</button>
+        </MainLayout>
+        )
+}
+
+About.getInitialProps = async () => {
+    const response = await fetch('http://localhost:4200/about')
+    const data = await response.json()
+
+    return {
+        title: data.title
+    }
+}
